ba9d0a319083afb84916e1df1a55a3438a424b11
SHEA <nacho.dieguez18@gmail.com>
2023-04-21 00:04:10 -0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/logo.png';
 
 const Header = () => {
   return (
@@ -7,7 +7,7 @@ const Header = () => {
       <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
           <a href='#'>
-            <img src={Logo} alt='logo'/>
+            <img className='h-30 w-40' src={Logo} alt='logo'/>
           </a>
           <button className='btn btn-sm'>Work with me</button>
         </div>
