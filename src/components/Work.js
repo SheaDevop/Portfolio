fc45317fe10c43f5e340f2519d4e33ffde2ceabb
SHEA <nacho.dieguez18@gmail.com>
2023-04-20 23:48:34 -0300

    work

--- src/components/Work.js
+++ src/components/Work.js
@@ -17,7 +17,7 @@ const Work = () => {
                 Work.
               </h2>
               <p className='max-w-sm mb-16'>Here you can check some of my latests projects</p>
-              <button className='btn btn-sm'>View all projects</button>
+              <button className='btn btn-sm'><a href='https://github.com/SheaDevop'>View all projects</a></button>
             </div>
             <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
