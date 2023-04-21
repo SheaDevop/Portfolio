0d0b06451e202a1ea35b6a7181ebaa395db99dcf
SHEA <nacho.dieguez18@gmail.com>
2023-04-20 21:21:58 -0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 const About = () => {
+
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
   return (
-    <div id='about' className='section'>
-      About
-    </div>
+    <section ref={ref} id='about' className='section'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+          <motion.div variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></motion.div>
+          <motion.div variants={fadeIn('left', 0.5)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1'>
+            <h2 className='h2 text-accent'>About me.</h2>
+            <h3 className='h3 mb-4 '>
+              I'm a Full-stack Developer, but mainly focused on the front-end side.
+            </h3>
+            <p className='mb-6'>I live in a continuous learning process and I'm currently looking for my first Job experience in the industry.</p>
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2 pt-1'>
+                  {
+                    inView ? (
+                      <CountUp start={0} end={3} duration={3}/>
+                    ) : null
+                  }
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Years of <br/>
+                  Learning experience
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-x-8 items-center'>
+              <button className='btn btn-lg'>Contact me</button>
+              <a href='#' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
