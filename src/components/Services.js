73631dfbc0e47bfe3fb822c11d0b0784e8ebd75d
SHEA <nacho.dieguez18@gmail.com>
2023-04-21 01:00:32 -0300

    services

--- src/components/Services.js
+++ src/components/Services.js
@@ -23,7 +23,7 @@ const Services = () => {
     <section id='services' className='section'>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row '>
-          <motion.div variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0'>
+          <motion.div variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 mb-12 lg:mb-0'>
             <h2 className='h2 text-accent mb-6'>What I Do.</h2>
             <h3 className='h3 max-w-[455px] mb-16 '>
               I'm a Front-end Developer with 3 years of learning experience.
